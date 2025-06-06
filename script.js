b381e4314af4b55a1e434eb79493f2ad718ccfa2
Guilherme Cardoso <129095506+Guilherme-Cardosoo@users.noreply.github.com>
2025-06-06 10:00:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+let iniciar = document.getElementById("iniciar");
+        iniciar.addEventListener("click",function(){
+            console.log("foi")
+            document.getElementById("page2").style.display="block";
+            document.getElementById("page1").style.display="none";
+        });
