eac65c388ed9da674a23c0c7711a9552fa660ad7
Guilherme Cardoso <129095506+Guilherme-Cardosoo@users.noreply.github.com>
2025-06-12 09:20:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,47 @@ let iniciar = document.getElementById("iniciar");
             document.getElementById("page2").style.display="block";
             document.getElementById("page1").style.display="none";
         });
+
+let timeInSeconds = 30;
+const timerElement = document.getElementById("timer");
+
+function updateTimer () {
+    const minutes = Math.floor(timeInSeconds / 60);
+    const seconds = timeInSeconds % 60;
+
+    timerElement.textContent =
+      `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+
+    if (timeInSeconds > 0) {
+        timeInSeconds--;
+        setTimeout(updateTimer, 1000);
+    } else {
+        alert("Tempo esgotado");
+    }
+}
+
+window.onload = updateTimer;
+
+let acertos = 0;
+const limeteAcertos = 3;
+const contador = document.getElementById("acertos");
+const bandeiras = document.querySelectorAll("#flags img");
+
+bandeiras.forEach(bandeira => {
+    bandeira.addEventListener("click", () => {
+        if (bandeira.classList.contains("clicado")) return;
+
+        bandeira.classList.add("clicado");
+
+        const correta = bandeira.getAttribute("data-correct") === "true";
+
+        if (correta) {
+            acertos++;
+            contador.textContent = `${acertos}/${limiteAcertos}`;
+        }
+
+        if (acertos === limeteAcertos) {
+            alert("Você acertou 3 bandeiras!");
+        }
+    })
+})
